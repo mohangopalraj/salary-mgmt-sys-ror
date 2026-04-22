fb5aab6596ba550b3507bf9f94fcedf137a043b4
Mohankumar <gmdctbe@gmail.com>
2026-04-22 20:22:17 +0530

    Add model and test case

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe Employee do
+    it "calculates average salary correctly" do
+      create(:employee, salary: 100)
+      create(:employee, salary: 200)
+
+      expect(Employee.average(:salary)).to eq(150)
+    end
+  end
 end
