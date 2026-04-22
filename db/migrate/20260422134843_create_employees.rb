9e9cdbbdd81cd872df203db9aecca64ae96824af
Mohankumar <gmdctbe@gmail.com>
2026-04-22 19:23:13 +0530

    Add Employee Model with validations

---
+++ db/migrate/20260422134843_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[8.1]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :job_title
+      t.string :country
+      t.integer :salary
+      t.string :department
+
+      t.timestamps
+    end
+  end
+end
